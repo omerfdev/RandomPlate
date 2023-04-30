09680bd75fd4785be26821bda4a0217c244eee6f
omerf <omerf@ALMALI>
2023-04-30 19:26:24 +0300

    Add project files.

---
+++ RandomPlate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace RandomPlate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of Code Length"); 
+            int x=int.Parse(Console.ReadLine());
+            Console.WriteLine(RandomPlateMethod(x));
+            Console.ReadLine();
+        }
+
+        private static string RandomPlateMethod(int x)
+        {
+            string code = "";
+            for (int i = 0; i < x; i++)
+            {
+                code += CodeValue(i % 2 == 0);
+            }
+            return code;
+        }
+         
+        private static char CodeValue(bool numeric)
+        {
+            Random rnd = new Random();  
+            if (numeric)  return (char)rnd.Next(48, 58); 
+            else return (char)rnd.Next(65,91);
+        }
+        
+    }
+}
