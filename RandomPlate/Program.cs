1c9e1fc88ffd4dc9510307dc38fc3700ea31f19b
omerfdev <omerfdev@gmail.com>
2023-07-08 13:49:27 +0300

    Commit

--- RandomPlate/Program.cs
+++ RandomPlate/Program.cs
@@ -3,52 +3,54 @@ using System.Threading.Channels;
 
 namespace RandomPlate
 {
-    public class Program
-    {
-        public delegate char CodeValueDelegate(bool numeric);
-        public static CodeValueDelegate codevalueDelegate = CodeValue;
-        public delegate string RandomPlateMethodDelegate(int y);
-        public static RandomPlateMethodDelegate randomplatemethodDelegate= RandomPlateMethod;
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Enter Number of Code Length"); 
-            int x=int.Parse(Console.ReadLine());
-            Console.WriteLine(randomplatemethodDelegate(x));
-            Console.ReadLine();
-        }
+	public class Program
+	{
+		public delegate char CodeValueDelegate(bool numeric);
+		public static CodeValueDelegate codevalueDelegate = CodeValue;
+		public delegate string RandomPlateMethodDelegate(int y);
+		public static RandomPlateMethodDelegate randomplatemethodDelegate = RandomPlateMethod;
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Enter Number of Code Length");
+			int x = int.Parse(Console.ReadLine());
+			Console.WriteLine(randomplatemethodDelegate(x));
+			Console.ReadLine();
+		}
 
-        public static string RandomPlateMethod(int x)
-        {
-            string code = "";
-            for (int i = 0; i < x; i++)
-            {
-                code += CodeValue(i % 2 == 0);
-            }
-            return code;
-        }
-         
-        public static char CodeValue(bool numeric)
-        {
-            Random rnd = new Random();
-            if (numeric)
-            {
-                int number=rnd.Next(2);
-             if(number==0)   return (char)rnd.Next(48,58);
-             else return (char)rnd.Next(95,96);
-            }
+		public static string RandomPlateMethod(int x)
+		{
+			string code = "";
+			for (int i = 0; i < x; i++)
+			{
+				code += CodeValue(i % 2 == 0);
+			}
+			return code;
+		}
 
-            else
-            {
-                int number = rnd.Next(3);
-                if (number==0) { return (char)rnd.Next(65, 91); }
-                else if (number==1){ return (char)rnd.Next(45,47); }
-                else if (number == 2) { return (char)rnd.Next(97, 122); }
-                else
-                {
-					return (char)rnd.Next(42,43);
+		public static char CodeValue(bool numeric)
+		{
+			Random rnd = new Random();
+			if (numeric)
+			{
+				int number = rnd.Next(2);
+				if (number == 0) return (char)rnd.Next(48, 58);
+				else return
+				(char)rnd.Next(65, 91);
+			}
+
+			else
+			{
+				int number = rnd.Next(3);
+				if (number == 0) { return (char)rnd.Next(95, 96); }
+				else if (number == 1) { return (char)rnd.Next(45, 47); }
+				else if (number == 2) { return (char)rnd.Next(97, 122); }
+				else
+				{
+					return (char)rnd.Next(42, 43);
 				}
-            }
-        }
-        
-    }
+			}
+		}
+
+
+	}
 }
